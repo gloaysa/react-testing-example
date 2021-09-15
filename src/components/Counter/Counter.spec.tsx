a7fd1b7f0a8d4612e39681f1f991781f00d8000b
Guillermo Loaysa <guillermo.loaysa.extern@diconium.com>
2021-09-15 18:22:19 +0200

    remove test from Counter.spec

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -1,16 +1,3 @@
-import Counter from 'components/Counter';
-import React from 'react';
-import { render, cleanup, screen, fireEvent } from 'utils/test-utils';
-
-afterEach(() => cleanup());
-
 describe('Testing the component', () => {
 
-  // Templating test
-  it('Counter', () => {
-    render(<Counter />);
-    expect(screen.getByTestId('count')).toBeInTheDocument();
-    fireEvent.click(screen.getByTestId('button'));
-    expect(screen.getByTestId('count')).toHaveTextContent('Count : 1');
-  });
 });
